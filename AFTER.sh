273ba8a2d83bb6238d9d4ce34fed2f9a7470cbf4
🇨🇦 AddisonYT04 🇨🇦 <evoker4mc@icloud.com>
2026-04-16 19:10:05 -0400

    Delete AFTER.sh

--- AFTER.sh
+++
@@ -1,6 +0,0 @@
-sudo pacman -S gnome gnome-extra xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm
-startx 
-exit
-sudo pacman -S gdm
-sudo systemctl enable gdm.service
-sudo systemctl start gdm.service
